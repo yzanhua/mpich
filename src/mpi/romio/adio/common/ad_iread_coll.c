e484a8d35fb3377a98faf8407eb0e0bea0277641
wkliao <wkliao@users.noreply.github.com>
2019-11-08 19:16:29 -0600

    Guard MPIX_Grequest_class with condition HAVE_MPIX_GREQUEST_CLASS
    * MPIX_Grequest_class is defined in MPICH's mpi.h, but may not be in
      other MPI implementations, such as OpenMPI

--- src/mpi/romio/adio/common/ad_iread_coll.c
+++ src/mpi/romio/adio/common/ad_iread_coll.c
@@ -8,6 +8,19 @@
 #include "mpiu_greq.h"
 #include "mpioimpl.h"
 
+#if !defined(MPI_IMPL_IS_MPICH) && !defined(HAVE_MPIX_GREQUEST_CLASS) && !defined(HAVE_MPI_GREQUEST_EXTENSIONS)
+void ADIOI_GEN_IreadStridedColl(ADIO_File fd, void *buf, int count,
+                                MPI_Datatype datatype, int file_ptr_type,
+                                ADIO_Offset offset, MPI_Request * request, int *error_code)
+{
+    static char myname[] = "ADIOI_GEN_IreadStridedColl";
+
+    *error_code = MPIO_Err_create_code(MPI_SUCCESS, MPIR_ERR_RECOVERABLE,
+                                       myname, __LINE__,
+                                       MPI_ERR_UNSUPPORTED_OPERATION, "**fileopunsupported", 0);
+}
+#else
+
 #ifdef MPL_USE_DBG_LOGGING
 #define RDCOLL_DEBUG 1
 #endif
@@ -525,6 +538,7 @@ static void ADIOI_GEN_IreadStridedColl_fini(ADIOI_NBC_Request * nbc_req, int *er
 
 static void ADIOI_Iread_and_exch(ADIOI_NBC_Request * nbc_req, int *error_code)
 {
+    MPI_Aint lb;
     ADIOI_Iread_and_exch_vars *vars = nbc_req->data.rd.rae_vars;
     ADIO_File fd = vars->fd;
     MPI_Datatype datatype = vars->datatype;
@@ -1291,3 +1305,4 @@ static int ADIOI_GEN_irc_wait_fn(int count, void **array_of_states,
   fn_exit:
     return errcode;
 }
+#endif
