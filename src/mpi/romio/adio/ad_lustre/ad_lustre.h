0b773530cfbc7fad6ed122bcf98823620e3ddeef
wkliao <wkliao@users.noreply.github.com>
2023-08-16 16:46:33 -0500

    Move ADIOI_LUSTRE_Calc_my_req
    ADIOI_LUSTRE_Calc_my_req is called only in file ad_lustre_wrcoll.c

--- src/mpi/romio/adio/ad_lustre/ad_lustre.h
+++ src/mpi/romio/adio/ad_lustre/ad_lustre.h
@@ -78,11 +78,6 @@ int ADIOI_LUSTRE_Docollect(ADIO_File fd, int contig_access_count,
                            ADIO_Offset * len_list, int nprocs);
 
 void ADIOI_LUSTRE_Get_striping_info(ADIO_File fd, int *striping_info, int mode);
-void ADIOI_LUSTRE_Calc_my_req(ADIO_File fd, ADIO_Offset * offset_list,
-                              ADIO_Offset * len_list, int contig_access_count,
-                              int *count_my_req_procs_ptr,
-                              int **count_my_req_per_proc_ptr,
-                              ADIOI_Access ** my_req_ptr, ADIO_Offset *** buf_idx_ptr);
 void ADIOI_LUSTRE_Free_my_req(int nprocs, int *count_my_req_per_proc,
                               ADIOI_Access * my_req, ADIO_Offset ** buf_idx);
 
