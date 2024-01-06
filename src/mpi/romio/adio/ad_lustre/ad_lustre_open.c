1a52b0a16580181c9f06ed72bdee39e7c3244512
Wei-keng Liao <wkliao@users.noreply.github.com>
2024-01-06 14:47:40 -0800

    guard codes for debugging with WKL_DEBUG

--- src/mpi/romio/adio/ad_lustre/ad_lustre_open.c
+++ src/mpi/romio/adio/ad_lustre/ad_lustre_open.c
@@ -71,12 +71,14 @@ void ADIOI_LUSTRE_Open(ADIO_File fd, int *error_code)
 
     MPI_Comm_rank(fd->comm, &myrank);
 
+#ifdef WKL_DEBUG
 if (myrank == 0) {
     if (fd->access_mode & ADIO_CREATE)
         printf("%4d: %s line %d - Lustre file create ---- %s\n",myrank,__func__,__LINE__,fd->filename);
     else
         printf("%4d: %s line %d - Lustre file open   ---- %s\n",myrank,__func__,__LINE__,fd->filename);
 }
+#endif
 
     if (fd->perm == ADIO_PERM_NULL) {
         old_mask = umask(022);
